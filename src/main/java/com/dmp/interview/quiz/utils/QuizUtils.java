a7b2289b0316d9f118e16255352b88256d397229
Jim Daehn <jdaehn@me.com>
2014-08-22 14:37:12 -0500

    Initial commit

---
+++ src/main/java/com/dmp/interview/quiz/utils/QuizUtils.java
@@ -0,0 +1,35 @@
+package com.dmp.interview.quiz.utils;
+
+/**
+ * @author jdaehn
+ * @since 8/21/2014 1:33 PM
+ */
+public class QuizUtils {
+    /**
+     * Convert the given character array into an int value, if possible.
+     *
+     * It is assumed that the given character array is representative of a C-style string, i.e., that
+     * it is terminated with the null character 0x0.
+     *
+     * @param stringInput a C-style character array representing a long value.
+     * @return the corresponding long value of the given input. If the given input is not terminated by the null
+     * character, return {@code Integer.MIN_VALUE}. If the given input does not represent a long value, return -1.
+     */
+    public static int convertToInt(char[] stringInput) {
+        return 0;
+    }
+
+    /**
+     * Tests the bit at {@code position} of the given {@code value}.
+     *
+     * It is assumed that the least significant bit is located at bit position 0.
+     * NOTE: You are not allowed to use any String functions.
+     *
+     * @param value the interrogated value
+     * @param position the bit-position of the given value
+     * @return true if the bit at the requested position is set (i.e., is value 1), false otherwise.
+     */
+    public static boolean testBit(int value, int position) {
+        return false;
+    }
+}
---
+++ src/test/java/com/dmp/interview/quiz/utils/QuizUtilsTest.java
@@ -0,0 +1,84 @@
+package com.dmp.interview.quiz.utils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuizUtilsTest {
+
+    private static char[] number1ToConvert;
+    private static char[] number2ToConvert;
+    private static char[] number3ToConvert;
+    private static char[] number4ToConvert;
+    private static char[] number5ToConvert;
+
+    private static int expectedInteger;
+    private static int actualInteger;
+
+    private static boolean expectedBoolean;
+    private static boolean actualBoolean;
+
+    @Before
+    public void setUp() throws Exception {
+        number1ToConvert = "1234".toCharArray();
+        number2ToConvert = "1234".concat("\0").toCharArray();
+        number3ToConvert = " 1".concat("\0").toCharArray();
+        number4ToConvert = "ABCD".toCharArray();
+        number5ToConvert = "ABCD".concat("\0").toCharArray();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        number1ToConvert = null;
+        number2ToConvert = null;
+        number3ToConvert = null;
+    }
+
+    @Test
+    public void testConvertToInt() throws Exception {
+        // number1ToConvert doesn't have null terminating character, so we expect Integer.MIN_VALUE
+        expectedInteger = Integer.MIN_VALUE;
+        actualInteger = QuizUtils.convertToInt(number1ToConvert);
+        assertEquals(expectedInteger, actualInteger);
+
+        // number2ToConvert is a valid number
+        expectedInteger = 1234;
+        actualInteger = QuizUtils.convertToInt(number2ToConvert);
+        assertEquals(expectedInteger, actualInteger);
+
+        // number3ToConvert is a valid number
+        expectedInteger = 1;
+        actualInteger = QuizUtils.convertToInt(number1ToConvert);
+        assertEquals(expectedInteger, actualInteger);
+
+        // number4ToConvert doesn't have null terminating character, so we expect Integer.MIN_VALUE
+        expectedInteger = Integer.MIN_VALUE;
+        actualInteger = QuizUtils.convertToInt(number1ToConvert);
+        assertEquals(expectedInteger, actualInteger);
+
+        // number5ToConvert while a valid hexadecimal value, isn't a valid decimal value
+        expectedInteger = -1;
+        actualInteger = QuizUtils.convertToInt(number1ToConvert);
+        assertEquals(expectedInteger, actualInteger);
+    }
+
+    @Test
+    public void testTestBit() throws Exception {
+        for (int value = Short.MIN_VALUE; value < Short.MAX_VALUE; value += 1) {
+            for (int position = 0; position < 32; position += 1) {
+                String binaryRepresentation = new StringBuilder(padLeft(Integer.toBinaryString(value))).reverse().toString();
+                expectedBoolean = (binaryRepresentation.charAt(position) == '1');
+                actualBoolean = QuizUtils.testBit(value, position);
+                assertEquals(expectedBoolean, actualBoolean);
+            }
+        }
+
+        System.out.println(padLeft(Integer.toBinaryString(Short.MAX_VALUE)));
+    }
+
+    private String padLeft(String number) {
+        return String.format("%32s", number).replace(' ', '0');
+    }
+}
